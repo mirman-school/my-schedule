dcb4d8a8492437eb2948d55ac4c93f668977c644
Eric <eryoon2019@mirmanmustangs.org>
2017-07-28 12:45:37 -0700

    Hello World, still not working

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import {render} from "react-dom";
 
+render(
+    <h1>Hello world!</h1>, document.getElementById("root")
+);
+
 const MySchedule = () => {
     return (
         <h1>My Schedule</h1>
--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: "babel-loader"
-      },
+      }
     ]
   },
   plugins: [
