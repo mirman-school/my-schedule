7139d783d82292072027b23e35da70f4e2b5697c
Eric <eryoon2019@mirmanmustangs.org>
2017-07-30 21:43:31 -0700

    Add semantic ui react
    And a button to go along with it

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import {render} from "react-dom";
+import {Button} from "semantic-ui-react";
 
 //import my components
 import Calendar from "./react_components/Calendar"
 
 render(
-    <Calendar/>, document.getElementById("root")
+    <div>
+    <Calendar/>
+    <Button>Click Here</Button></div>, document.getElementById("root")
 );
 /*
 const MySchedule = () => {
