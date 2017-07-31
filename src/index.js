e3aa7f7b05e3d028ae107da05bb14fa769268294
Eric <eryoon2019@mirmanmustangs.org>
2017-07-30 21:36:49 -0700

    Add calendar component
    I'll probably ACTUALLY make a calendar tomorrow

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import {render} from "react-dom";
 
-render(
-    <h1>Hello world!</h1>, document.getElementById("root")
-);
+//import my components
+import Calendar from "./react_components/Calendar"
 
+render(
+    <Calendar/>, document.getElementById("root")
+);
+/*
 const MySchedule = () => {
     return (
         <h1>My Schedule</h1>
     )
 }
+*/
---
+++ src/react_components/Calendar.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class Calendar extends React.Component {
+  render() {
+    return <h1>When I grow up, I want to be a calendar!</h1>;
+  }
+};
