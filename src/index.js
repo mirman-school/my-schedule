020e6d4c1f31926b8d2ee21f201cf0647d3a299a
Eric <eryoon2019@mirmanmustangs.org>
2017-07-31 17:14:15 -0700

    Calendar header
    displays the days

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import React from "react";
 import {render} from "react-dom";
-import {Button} from "semantic-ui-react";
-
 //import my components
 import Calendar from "./react_components/Calendar"
 
 render(
-    <div>
-    <Calendar/>
-    <Button>Click Here</Button></div>, document.getElementById("root")
+    <Calendar days="4"/>, document.getElementById("root")
 );
 /*
 const MySchedule = () => {
--- src/react_components/Calendar.js
+++ src/react_components/Calendar.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import {Table} from 'semantic-ui-react';
 export default class Calendar extends React.Component {
   render() {
-    return <h1>When I grow up, I want to be a calendar!</h1>;
+    
+    const days = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+    var headerCells = [];
+    for(var day = 0; day < this.props.days; day++){
+      const letter = days[day % days.length];
+      headerCells.push(<Table.HeaderCell>Day {letter}</Table.HeaderCell>);
+    }
+    var header = (
+      <Table.Header>
+        <Table.Row>
+          {headerCells}
+        </Table.Row>
+      </Table.Header>
+    );
+    var table = (
+      <Table>
+        {header}
+      </Table>
+    );
+    return table;
   }
 };
