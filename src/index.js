cec6c44bd680de8fd67aaad04a01fcead225de7b
Eric <eryoon2019@mirmanmustangs.org>
2017-08-04 20:43:26 -0700

    Now using fetch

--- src/index.js
+++ src/index.js
@@ -2,35 +2,45 @@ import React from "react";
 import {render} from "react-dom";
 //import my components
 import Calendar from "./react_components/Calendar"
-import {testdata} from "../testdata.json"
 
-var data = testdata;
-var maxRows = 0;
-Object.values(data).map((val) => {
-    if(Object.keys(val).length > maxRows)
-        maxRows = Object.keys(val).length;
-});
-//The array should be rectangular, but do this just in case...
 
-render(
-    <Calendar days={Object.keys(data).length} periods={maxRows}/>, document.getElementById("root")
-);
-Object.keys(data).map((val, index) => {
-    document.getElementById("cal-head-" + index).innerHTML = "<b>" + val + "</b>";
-});
-Object.values(data).map((day, dayI) => {
-    Object.values(day).map((val, period) => {
-        document.getElementById("cal-" + dayI + "-" + period).innerHTML = val.name;
+fetch('testdata.json', {
+	method: 'get'
+}).then(function(response) {
+	return response.json();
+}).then(function(json){
+    console.log(json);
+    var data = json.testdata;
+
+
+
+    var maxRows = 0;
+    Object.values(data).map((val) => {
+        if(Object.keys(val).length > maxRows)
+            maxRows = Object.keys(val).length;
     });
+    //The array should be rectangular, but do this just in case...
+
+    render(
+        <Calendar days={Object.keys(data).length} periods={maxRows}/>, document.getElementById("root"), () => {
+
+
+            Object.keys(data).map((val, index) => {
+                document.getElementById("cal-head-" + index).innerHTML = "<b>" + val + "</b>";
+            });
+            Object.values(data).map((day, dayI) => {
+                Object.values(day).map((val, period) => {
+                    document.getElementById("cal-" + dayI + "-" + period).innerHTML = val.name;
+                });
+            });
+        
+        
+        
+        });
     
+
+
+
+
+
 });
-
-
-
-/*
-const MySchedule = () => {
-    return (
-        <h1>My Schedule</h1>
-    )
-}
-*/
