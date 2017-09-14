6c9b20178a001f1a4f5470f6ede59aaa4bdc8d2d
mirman-mtaggart <mtaggart@mirman.org>
2017-09-14 14:27:46 -0700

    Firebase begin

---
+++ src/config/firebase.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyBUCIjDLqsFxem4gxinzei46UeRHCIppLs",
+    authDomain: "my-schedule-deb36.firebaseapp.com",
+    databaseURL: "https://my-schedule-deb36.firebaseio.com",
+    projectId: "my-schedule-deb36",
+    storageBucket: "my-schedule-deb36.appspot.com",
+    messagingSenderId: "1087688681877"
+}
