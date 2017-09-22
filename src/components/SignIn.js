e7c86eb951b7120fd159d557b904a5f56ff9d28b
Eric <eryoon2019@mirmanmustangs.org>
2017-09-21 17:57:48 -0700

    Nevermind I give up on trying to make my own spacer

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Header, Button } from 'semantic-ui-react';
 import Center from "react-center";
-import Spacer from "./Spacer";
 
 export default class SignIn extends React.Component {
 
@@ -9,7 +8,6 @@ export default class SignIn extends React.Component {
         return(
             <div>
                 <Center><Header as="h1">Hello.</Header></Center>
-                <Spacer height="200px"/>
                 <Center><Header as="h2">Sign in to get started.</Header></Center>
                 <Center><Button>Replace me with an official Sign In With Google button!</Button></Center>
             </div>
--- src/components/Spacer.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Spacer(height){
-
-    return <div style={{"paddingBottomwonton": height}} />;
-}
-export default Spacer;
