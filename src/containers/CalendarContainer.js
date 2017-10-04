f612b05e0f6c5131ffaf0b6db19dd21a1f52af62
mirman-mtaggart <mtaggart@mirman.org>
2017-10-04 14:14:23 -0700

    Start firestore

--- src/containers/CalendarContainer.js
+++ src/containers/CalendarContainer.js
@@ -17,6 +17,8 @@ export default class CalendarContainer extends React.Component {
         const firebaseConfig = config.firebase;
         firebase.initializeApp(firebaseConfig);
 
+        const db = firebase.firestore();
+
         fetch("../../testdata.json", {
             method: "get"
         }).then((response) => {
