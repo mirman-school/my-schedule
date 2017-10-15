9dcffd61f4992817da541eb5de8e1162fb2960e7
Eric <eryoon2019@mirmanmustangs.org>
2017-10-15 10:15:21 -0700

    Try to do auth again... getting closer
    I think I need to move the auth to a background script

--- src/containers/CalendarContainer.js
+++ src/containers/CalendarContainer.js
@@ -26,28 +26,59 @@ export default class CalendarContainer extends React.Component {
         //const db = firebase.firestore();
         var provider = new firebase.auth.GoogleAuthProvider();
         var credential;
-        if(false){ //if cookie
-            credential = chrome.storage.sync.get("cred", function(res){
-                credential = res.cred;
-            });
-        }else{
-            //else get cred from chrome
-            chrome.identity.getAuthToken({ 'interactive': true }, function(token) {
-                credential = firebase.auth.GoogleAuthProvider.credential(null, token);
-            });
-            chrome.storage.sync.set({'cred': credential}, function() {
-                console.log("My Schedule firebase cred saved in chrome.storage");
-            });
-        }
-        console.log(credential);
-        firebase.auth().signInWithCredential(credential);
-        
-        
-        
-        
+        var data;
+        chrome.storage.sync.get("cred", function(res){
+            console.log("TRYING to get cred from chrome storage...")
+            if(chrome.runtime.error){
+                console.error("Whoa there! We found a chrome runtime error in storage.get!");
+                data = null;
+            }else{
+                console.log("Res:");
+                console.log(res);
+                console.log("Data:");
+                data = res.cred;
+                console.log(data);
+                console.log("Data get success. It still may be null at this point.");
+            }
 
-
+            
+                
+            if(data == null || data == undefined){ //if there is no storage
+                //else get cred from chrome
+                console.log("Data is null. Now getting auth token from chrome identity...");
+                chrome.identity.getAuthToken({ 'interactive': false }, function(token) {
+                    console.log("Successfully retrieved chrome auth token. Token:");
+                    console.log(token);
+                    credential = firebase.auth.GoogleAuthProvider.credential(null, token);
+                    chrome.storage.sync.set({"cred": credential}, function() {
+                        console.log("Extracted firebase cred, now saving the cred in chrome storage");
+                        if(chrome.runtime.error){
+                            console.error("Whoa there! We found a chrome runtime error in storage.set!");
+                        }
+                        console.log("Credential:");
+                        console.log(credential);
+                        firebase.auth().signInWithCredential(credential);
+                    });
+                });
+            }else{
+                //use data
+                credential = data;
+                console.log("If the following is true, then something went REALLY wrong...");
+                console.log(data == undefined);
+                console.log("Data is not null. Using the data to sign in.");
+                console.log("Credential:");
+                console.log(credential);
+                firebase.auth().signInWithCredential(credential);
+            }
+            
+        });
+            
     }
+
+        
+        
+        
+    
 
 
 
