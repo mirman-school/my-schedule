a9408e16962169426cfade40bd4341b707369a67
Eric <eryoon2019@mirmanmustangs.org>
2017-12-01 11:28:23 -0800

    Did auth right this time
    But it's still not working. I dont know how to get user id from GetToken

--- src/containers/CalendarContainer.js
+++ src/containers/CalendarContainer.js
@@ -5,7 +5,8 @@ import SourceViewButton from "../components/SourceViewButton";
 import AddDay from "../components/AddDay";
 import AddPeriod from "../components/AddPeriod";
 import config from "../config";
-import firebase from "firebase";
+import * as firebase from "firebase";
+import "firebase/firestore";
 
 export default class CalendarContainer extends React.Component {
 
@@ -18,28 +19,45 @@ export default class CalendarContainer extends React.Component {
 
     componentDidMount() {
     const firebaseConfig = config.firebase;
-    var data = null;
-    chrome.storage.sync.get("cred", function(res){
-    if(chrome.runtime.error){
-        console.error("CalCon: Whoa there! We found a chrome runtime error in storage.get!");
-        data = null;
-    }else{
-        data = res.cred; 
-    }
 
-    if(data !== null && data !== undefined){
-        firebase.initializeApp(firebaseConfig);
-        var credential = firebase.auth.GoogleAuthProvider.credential(null, data); //Wow. I spent 3 hours on this, and all I had to do was add "null, "
-        firebase.auth().signInWithCredential(credential).catch(function(err){ //https://github.com/firebase/quickstart-js/issues/133... oh no now i'm getting this... https://github.com/prescottprue/react-redux-firebase/issues/87
+    firebase.initializeApp(firebaseConfig);
+    chrome.identity.getAuthToken({ 'interactive': true }, function(token) {
+        console.log(token);
+        var credential = firebase.auth.GoogleAuthProvider.credential(null, token);
+        console.log(credential);
+        firebase.auth().signInWithCredential(credential).then(function(result){
+            console.log("Signed in!");
+            var token = result.accessToken;
+            var user = result.user;
+            var uid = user.uid;
+            var db = firebase.firestore();
+            var docname = "hello";
+            var docRef = db.collection(docname).doc(uid);
+        
+            docRef.get().then(function(doc) {
+                if (doc.exists) {
+
+                }else{
+                    console.log("No such document! Creating one.");
+                    db.collection(docname).doc(uid).set({
+                        "ilikedata": true
+                    }).then(function(){
+                        console.log("Document created!");
+                    });
+                }
+
+            }).catch(function(err){
+                console.error("Error getting doc: " + err);
+            });
+        }).catch(function(error){ //https://github.com/firebase/quickstart-js/issues/133... oh no now i'm getting this... https://github.com/prescottprue/react-redux-firebase/issues/87
             var errorCode = error.code;
             var errorMessage = error.message;
             var email = error.email;
-            var credential = error.credential;
-            console.error('Failed to login user ' + email + " with cred " + credential + ". Code: " + errorCode + ", msg: " + errorMessage);
+            var errcred = error.credential;
+            console.error("Failed to login. Code: " + errorCode + ", msg: " + errorMessage);
         });
-    }else{
-        console.error("CalCon: Chrome storage data is NULL! You'll have to tell background.js...");
-    }
+
+        
 
     });
 }
